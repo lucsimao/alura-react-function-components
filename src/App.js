6726de6fcb8760905cea6f68acec7169631a7951
lucsimao <lucasimao64@gmail.com>
2022-02-13 14:13:20 -0300

    feat: add Container to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
+import { Container } from '@material-ui/core';
 import FormularioCadastro from './FormularioCadastro/FormularioCadastro';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Container component="article" maxWidth="sm">
         <h1>Formulário de cadastro</h1>
         <FormularioCadastro />
-      </Fragment>
+      </Container>
     );
   }
 }
