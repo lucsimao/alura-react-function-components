6dd68f10b61a63610ac09bbdb7ba368500d41816
lucsimao <lucasimao64@gmail.com>
2022-02-14 22:09:49 -0300

    refactor: update formularios

--- src/FormularioCadastro/FormularioCadastro.jsx
+++ src/FormularioCadastro/FormularioCadastro.jsx
@@ -3,29 +3,20 @@ import React, { useState } from 'react';
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
-import { Typography } from '@material-ui/core';
 
 function FormularioCadastro({ aoEnviar, validarCpf }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf} />;
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar} />;
-      default:
-        return <Typography>Erro ao selecionar formulário</Typography>;
-    }
-  }
-
-  return <>{formularioAtual(etapaAtual, { aoEnviar, validarCpf })}</>;
+  return <>{formularios[etapaAtual]}</>;
 }
 
 export default FormularioCadastro;
